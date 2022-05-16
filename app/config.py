9ebc41f8b27e6b76c7cc1f2ccf1a814be76587a7
TzBlitz <Kenthonyg26@gmail.com>
2022-05-16 18:34:28 -0400

    created one file and put in old api key.

---
+++ app/config.py
@@ -0,0 +1,3 @@
+api_key = 'RGAPI-59374883-1cf2-4809-92ef-98b976f0f7ba'
+aws_access_key_id =''
+aws_secret_access_key = ''
--- config.py
+++
@@ -1,3 +0,0 @@
-api_key = ''
-aws_access_key_id =''
-aws_secret_access_key = ''
